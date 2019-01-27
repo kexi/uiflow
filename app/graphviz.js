e059c58ed9b4f5227e41e66689bbf4f324dc66bc
Kei Nakayama <kei.of.nakayama@gmail.com>
2019-01-27 16:14:47 +0900

    fix

--- app/graphviz.js
+++ app/graphviz.js
@@ -1,6 +1,11 @@
 var viz = require("viz.js");
 var through2 = require("through2");
 var spawnStream = require("spawn-stream");
+var r = require('viz.js/full.render.js');
+var Module = r.Module;
+var render = r.render;
+
+
 var graphviz = module.exports = function(format) {
     if (format == "png") {
         return function() {
@@ -12,9 +17,14 @@ var graphviz = module.exports = function(format) {
     if (format == "svg") {
         return function() {
             return through2(function(chunk, enc, callback) {
-                var svg = viz(String(chunk));
-                this.push(svg);
-                callback();
+                var v2 = new viz({Module: Module, render: render});
+                var that = this;
+                v2.renderSVGElement(String(chunk)).then(function(element) {
+                    that.push(element);
+                    callback();
+                }).catch(function(error) {
+                    console.log(error);
+                });
             });
         }
     }
